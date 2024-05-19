eb5b0d28ee85696878c46fa1f310261bf6a3b5b7
BTD <169353918+btd-git@users.noreply.github.com>
2024-05-19 20:00:31 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
 clear
 yay -Syy
-yay --noconfirm -S hyprland-git
-sudo pacman --noconfirm -S kitty alacritty sddm fastfetch dunst polkit-kde-agent xdg-desktop-portal-wlr xdg-desktop-portal-hyprland qt5-wayland qt6-wayland hyprpaper hypridle hyprlocks waybar rust rofi-wayland neofetch fastfetch btop htop cliphist 
+yay --noconfirm -S hyprland-git hyprpicker
+sudo pacman --noconfirm -S kitty alacritty sddm fastfetch dunst polkit-kde-agent xdg-desktop-portal-wlr xdg-desktop-portal-hyprland qt5-wayland qt6-wayland hyprpaper hypridle hyprlock waybar rust rofi-wayland neofetch fastfetch btop htop cliphist 
