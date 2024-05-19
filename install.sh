98b31c30d10d623f5ef96f942ab1045104f82127
BTD <169353918+btd-git@users.noreply.github.com>
2024-05-19 19:10:20 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/bash
+clear
+yay -Syy
+yay --noconfirm -S gdb ninja gcc cmake meson libxcb xcb-proto xcb-util xcb-util-keysyms libxfixes libx11 libxcomposite xorg-xinput libxrender pixman wayland-protocols cairo pango seatd libxkbcommon xcb-util-wm xorg-xwayland libinput libliftoff libdisplay-info cpio tomlplusplus hyprlang hyprcursor
+echo "BASE DEPENDENCIES INSTALLED. NEEDED TO BUILD Hyprland"
+sleep 3
+git clone --recursive https://github.com/hyprwm/Hyprland
+cd Hyprland
+make -j4 all && sudo make install
+sudo pacman --noconfirm -S kitty alacritty sddm btop htop fastfetch neofetch dunst polkit-kde-agent xdg-desktop-portal-wlr xdg-desktop-portal-hyprland qt5-wayland qt6-wayland hyprpaper hypridle hyprlocks
